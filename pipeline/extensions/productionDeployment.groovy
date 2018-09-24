b8e9f921f21ca6a89991e2b92db0c21ccf6aea8c
Ekta Jha <ekta.jha@sap.com>
2018-09-24 17:52:50 +0200

    Change(-)
    Change-Id: I6d10e1ccda7cd44678fcbb06622d61e1b24ca2bd
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -1,14 +1,9 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
-    dockerExecuteOnKubernetes(containerMap: ['lachlanevenson/k8s-kubectl': 'kubectl', 'devth/helm': 'helm']){
-       container('kubectl')  {                 
-                    sh "kubectl get pods"
-       }
-       container('helm'){
-         sh "helm init --upgrade"
-         sh "sleep 40"
-         sh "helm upgrade --install --force addrbook --set imageversion=v1 addressbook"
-       }
-   }
+    
+     export ON_K8S=true
+     dockerExecute(script: this, dockerImage: 'lachlanevenson/k8s-kubectl'){                
+        sh "kubectl get pods"
+    }
 }
 
 return this
