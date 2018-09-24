e5cbb01ebef236ab41e363d65b2b87327e3faad3
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-24 20:55:41 +0200

    Update productionDeployment.groovy

--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -1,7 +1,11 @@
-def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {  
-    container('kubectl') {
-      sh "kubectl get pods"
+def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
+    
+     dockerExecute(script: this, dockerImage: 'lachlanevenson/k8s-kubectl'){ 
+       sh  "cp config ~/.kube/config"
+       sh  "kubectl config get-contexts"             
+       sh  "kubectl get pods"
     }
+    
 }
 
 return this
