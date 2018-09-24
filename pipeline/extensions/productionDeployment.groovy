192d32d8a515efa4d868b6a6fd715c59d0ee1753
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-24 19:02:33 +0200

    Update productionDeployment.groovy

--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -1,9 +1,7 @@
-def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
-    
+def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {  
     container('kubectl') {
       sh "kubectl get pods"
     }
-
 }
 
 return this
