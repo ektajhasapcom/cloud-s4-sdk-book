bb183daf43b69bf8f33729c9dfe0a292923d333a
Ekta Jha <ekta.jha@sap.com>
2018-09-24 18:01:41 +0200

    -
    Change-Id: I5bf685d22384f23bd54323438152b4ac494a9a8b
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -1,6 +1,5 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
-     export ON_K8S=true
      dockerExecute(script: this, dockerImage: 'lachlanevenson/k8s-kubectl'){                
         sh "kubectl get pods"
     }
