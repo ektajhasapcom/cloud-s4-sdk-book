48e5ce4a0adb9f6fa2dc0e38aeb6c48fc95a307d
Ekta Jha <ekta.jha@sap.com>
2018-09-26 16:26:18 +0200

    -
    Change-Id: Id448c741c75410d7c9b3ad737a9340cfaebd6144
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -4,18 +4,15 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
     def  tag = "${env.BRANCH_NAME}.${env.BUILD_NUMBER}"
     
     originalStage()
+    
+    sh "docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker ubuntu:latest bash"
      
-    dockerExecute(script: this, dockerImage: 'devopshackbo/docker-machine:alpine'){ 
+   
     
-          sh "docker-machine ls"
-          sh "docker-machine env default"   
-          sh "eval \$(docker-machine env default)"
-    
-          withCredentials([usernamePassword(credentialsId: 'dockerCredentialId', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerCredentialId', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
                   sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
                   sh "docker build -t ${imageName}:${tag} ."
                   sh "docker push ${imageName}:${tag}"     
-          }
     }
     
 }
