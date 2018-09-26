63f158226e17c238e17beb8a07e60d9239f4d496
Ekta Jha <ekta.jha@sap.com>
2018-09-26 14:22:19 +0200

    -
    Change-Id: I92891a5e2784e87bd8a613206121b8d3e80ea3aa
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -5,6 +5,8 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
     
     originalStage()
     
+     sh "docker run -v /var/run/docker.sock:/var/run/docker.sock" -ti docker:dind
+    
      withCredentials([usernamePassword(credentialsId: 'dockerCredentialId', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
                   sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
                   sh "docker build -t ${imageName}:${tag} ."
