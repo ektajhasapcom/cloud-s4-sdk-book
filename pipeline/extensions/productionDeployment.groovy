17e031054364e5acbb98e5f2b065625423956743
Ekta Jha <ekta.jha@sap.com>
2018-09-26 09:49:51 +0200

    Change(helm)
    Change-Id: Iac072eeeae64f4c097d7bf73df1e63ac29d48726
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -11,7 +11,13 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
             ]]){
                  sh "kubectl --kubeconfig=$KUBECONFIG"
                  sh "kubectl get pods"
-                 sh "kubectl apply -f deployment.yaml"
+                 sh "sudo apt-get install wget"
+                 sh "wget https://storage.googleapis.com/kubernetes-helm/helm-v2.10.0-linux-amd64.tar.gz > /dev/null 2>&1"
+                 sh "tar -zxvf helm-v2.10.0-linux-amd64.tar.gz > /dev/null 2>&1"
+                 sh "sudo mv linux-amd64/helm /usr/local/bin/helm > /dev/null 2>&1"
+                 sh "helm init --upgrade"
+                 sh "sleep 40"
+                 sh "helm upgrade --install --force addrbook --set imageversion=$tag addressbook"            
               }
     }
 
