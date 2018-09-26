0835f38636293109a17a44c6c02ce344acf7dd88
Ekta Jha <ekta.jha@sap.com>
2018-09-26 11:30:46 +0200

    Change(Helm-Kubectl)
    Change-Id: I05b4428db78321bfa5fe9b023f2b7f8da6d93e23
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -1,9 +1,8 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
      def  tag = "${env.BRANCH_NAME}.${env.BUILD_NUMBER}"
-     def imageName = "ektajha/addressbooklatest"
      
-     dockerExecute(script: this, dockerImage: 'lachlanevenson/k8s-kubectl'){ 
+     dockerExecute(script: this, dockerImage: 'ektajha/k8shelm:v1'){ 
         withCredentials([[
                 $class: 'FileBinding',
                 credentialsId: 'k8s-credentials',
@@ -11,10 +10,6 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
             ]]){
                  sh "kubectl --kubeconfig=$KUBECONFIG"
                  sh "kubectl get pods"
-                 sh "apt-get install wget"
-                 sh "wget https://storage.googleapis.com/kubernetes-helm/helm-v2.10.0-linux-amd64.tar.gz > /dev/null 2>&1"
-                 sh "tar -zxvf helm-v2.10.0-linux-amd64.tar.gz > /dev/null 2>&1"
-                 sh "sudo mv linux-amd64/helm /usr/local/bin/helm > /dev/null 2>&1"
                  sh "helm init --upgrade"
                  sh "sleep 40"
                  sh "helm upgrade --install --force addrbook --set imageversion=$tag addressbook"            
