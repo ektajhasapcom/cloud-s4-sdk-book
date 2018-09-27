c59032ce24da54836b944837da405447fb59f6d4
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 22:54:03 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -2,17 +2,17 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
    
  
   podTemplate(label     : 'docker-node',
-            containers: [containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [
-                  hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
-             hostPathVolume(hostPath: '/home/jenkins/workspace/', mountPath: '/home/jenkins/workspace/')]) {
+            containers: [containerTemplate(name: 'maven', image: 'maven', ttyEnabled: true, command: 'cat'),
+                         containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [
+                  hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
              node('docker-node') {
+                container('maven') {
+                    sh  "ls -lrt"
+                    unstashFiles script: this, stage: stageName
+                    sh "mvn clean install -Dmaven.test.failure.ignore=true" 
+                }
                 container(name: 'docker') {
                     try {
-                         sh  "ls -lrt"
-                         unstashFiles script: this, stage: stageName
-                         dockerExecute(script:this, dockerImage:'maven'){
-                             sh "mvn clean install -Dmaven.test.failure.ignore=true"   
-                         }
                          sh "docker version"
                          sh "ls -lrt"
                          sh "docker build -t javaim:v1 application/Dockerfile"
