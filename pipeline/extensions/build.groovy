3ba0dfb58b99306f77e1235f732417dcc1635f7b
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 17:24:06 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,13 +1,18 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
 	
     
- podTemplate(label     : 'pod-hugo-app',
+  podTemplate(label     : 'pod-hugo-app',
            containers: [
-		   containerTemplate(name: 'dind-daemon', image: 'docker:dind', privileged: true)
-	          
-	       ]) {
+		   containerTemplate(name: 'dind-daemon', image: 'docker:dind', ttyEnabled: true, command: 'cat', privileged: true,
+				    volumes: [secretVolume(secretName: 'docker-graph-storage', mountPath: '/var/lib/docker')])
+	           ,
+		   containerTemplate(name: 'docker-cmds', image: 'docker', ttyEnabled: true, command: 'cat', 
+		          envVars: [envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')]		    
+	              )
+	           ], 
+		   volumes: [emptyDirVolume(mountPath: '/var/lib/docker')]) {
              node('pod-hugo-app') {
-                container(name: 'dind-daemon') {
+                container(name: 'docker-cmds') {
                     try {
                         sh "docker version"
                         sh "docker build ."
