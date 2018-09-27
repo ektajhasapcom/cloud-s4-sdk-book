4ec6c6a9cfc1afc0b6551ebfc1a99275658ca770
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 16:44:59 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -2,7 +2,7 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
     
   podTemplate(label     : 'pod-hugo-app',
            containers: [
-		   containerTemplate(name: 'dind-daemon', image: 'docker:dind', ttyEnabled: true, command: 'cat', args: '/bin/sh -c', privileged: true
+		   containerTemplate(name: 'dind-daemon', image: 'docker:dind', workingDir: '/root/', ttyEnabled: true, command: 'cat', privileged: true
 		  )
 	          
 	       ],
