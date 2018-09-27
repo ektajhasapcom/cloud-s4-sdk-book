a31069e453c287832b51785dc441c5483e9ebe77
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 15:20:45 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -4,7 +4,8 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
   podTemplate(label     : 'pod-hugo-app',
            containers: [
 		   containerTemplate(name: 'dind-daemon', image: 'docker:dind', ttyEnabled: true, command: 'cat', privileged: true,
-				    volumes: [secretVolume(secretName: 'docker-graph-storage', mountPath: '/var/lib/docker')])
+				    volumes: [secretVolume(secretName: 'docker-graph-storage', mountPath: '/var/lib/docker')],
+				    ports: [portMapping(containerPort: 2375, hostPort: 2375)])
 	           ,
 		   containerTemplate(name: 'docker-cmds', image: 'docker', ttyEnabled: true, command: 'cat', 
 		          envVars: [envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')]		    
