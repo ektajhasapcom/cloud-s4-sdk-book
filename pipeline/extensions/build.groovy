a3bf16d87f2dfbbf1416461784a15e559d6f576f
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 13:17:43 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,12 +1,24 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
+   
     
-     sh "docker run -t -i --privileged  docker:1.12.6-dind"
-     
-     sh "docker version"
-     
-     dockerExecute(script: this, dockerImage: 'docker:1.12.6'){ 
-        sh "docker version"
+    podTemplate(label: 'pod-hugo-app', containers: [
+    containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat',
+        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
+  ]) {
+
+    node('pod-hugo-app') {
+
+            container('docker') {
+                stage('Docker Build & Push Current & Latest Versions') {
+                    sh ("docker build -t ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER} .")
+                    sh ("docker push ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER}")
+                    sh ("docker tag ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER} ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:latest")
+                    sh ("docker push ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:latest")
+                }
+            }
+        }        
     }
+  }
 }
 
 return this
