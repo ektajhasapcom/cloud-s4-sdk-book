87942015d60d511b7eb206f2e3fd07474d8336f4
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 12:52:41 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,6 +1,6 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
-     docker run -t -i --privileged  docker:1.12.6-dind
+     sh "docker run -t -i --privileged  docker:1.12.6-dind"
      
      sh "docker version"
      
