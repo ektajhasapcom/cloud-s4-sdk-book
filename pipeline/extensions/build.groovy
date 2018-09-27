876876132cd094222966c365efef391393a067cd
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 13:40:55 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -2,8 +2,8 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
    
     
   podTemplate(label     : 'pod-hugo-app',
-            containers: [containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [secretVolume(secretName: 'docker-config', mountPath: '/tmp'),
-                  hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock') {
+            containers: [containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [
+                  hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
              node('pod-hugo-app') {
                 container(name: 'docker') {
                     try {
