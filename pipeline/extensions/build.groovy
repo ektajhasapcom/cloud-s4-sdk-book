296907ef6f41da73abbfe84d9773866c32c45165
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 14:39:38 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -2,9 +2,12 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
    
     
   podTemplate(label     : 'pod-hugo-app',
-           containers: [containerTemplate(name: 'docker-cmds', image: 'docker:1.12.6', ttyEnabled: true, command: 'cat', 
-		    envVars: [containerEnvVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')]),
-		   containerTemplate(name: 'dind-daemon', image: 'docker:1.12.6-dind', ttyEnabled: true, command: 'cat', privileged: true)], 
+           containers: [
+		   containerTemplate(name: 'dind-daemon', image: 'docker:1.12.6-dind', ttyEnabled: true, command: 'cat', privileged: true)
+	           ,
+		   containerTemplate(name: 'docker-cmds', image: 'docker:1.12.6', ttyEnabled: true, command: 'cat', 
+		    envVars: [containerEnvVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')])
+	           ], 
 		   volumes: [emptyDirVolume(mountPath: '/var/lib/docker')]) {
              node('pod-hugo-app') {
                 container(name: 'dind-daemon') {
