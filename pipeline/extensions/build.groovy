eb5324cf754ac6fd1d83fc1affee3d2973c08172
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 20:18:11 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,18 +1,16 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
    
+  unstashFiles script: this, stage: stageName
+  dockerExecute(script:this, dockerImage:'maven'){
+       sh "mvn clean install"   
+  }
+   
     
-  podTemplate(label:'pod-hugo-app',
-           containers: [
-		   containerTemplate(name: 'dind-daemon', image: 'docker:dind', privileged: true,
-				    volumes: [secretVolume(secretName: 'docker-graph-storage', mountPath: '/var/lib/docker')])
-	           ,
-		   containerTemplate(name: 'docker-cmds', image: 'docker',   
-		          envVars: [envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')]		    
-	              )
-	           ], 
-		   volumes: [emptyDirVolume(mountPath: '/var/lib/docker')]) {
-             node('pod-hugo-app') {
-                container('docker-cmds') {
+  podTemplate(label     : 'docker-node',
+            containers: [containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [
+                  hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+             node('docker-node') {
+                container(name: 'docker') {
                     try {
                         sh "docker version"
                         sh "docker build ."
