ad473457cbe6d8ae7501c2c159bb5df6a3c46884
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 22:46:48 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,21 +1,20 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
    
-  sh  "ls -lrt"
-  unstashFiles script: this, stage: stageName
-  dockerExecute(script:this, dockerImage:'maven'){
-       sh "mvn clean install -Dmaven.test.failure.ignore=true"   
-  }
-   
-  sh  "ls -lrt"
+ 
   podTemplate(label     : 'docker-node',
             containers: [containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [
                   hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
              node('docker-node') {
                 container(name: 'docker') {
                     try {
-                        sh "docker version"
-                        sh "ls -lrt"
-                        sh "docker build -t javaim:v1 application/Dockerfile"
+                         sh  "ls -lrt"
+                         unstashFiles script: this, stage: stageName
+                         dockerExecute(script:this, dockerImage:'maven'){
+                             sh "mvn clean install -Dmaven.test.failure.ignore=true"   
+                         }
+                         sh "docker version"
+                         sh "ls -lrt"
+                         sh "docker build -t javaim:v1 application/Dockerfile"
                     } finally {
                         echo "Finally"
                      }
