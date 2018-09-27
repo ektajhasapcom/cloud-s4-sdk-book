670b5cda8c3f8e405b27be65fd5ee03229ea0f3a
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 11:46:38 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,19 +1,8 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
-     dockerExecute(script: this, dockerImage: 'ektajha/k8shelm:v1'){ 
-        withCredentials([[
-                $class: 'FileBinding',
-                credentialsId: 'k8s-credentials',
-                variable: 'KUBECONFIG'
-            ]]){
-                 sh "kubectl --kubeconfig=$KUBECONFIG"
-                 sh "kubectl apply -f docker.yaml"
-                 sh "sleep 40"
-                 sh "kubectl get pods"
-                 sh "docker version"           
-              }
+     dockerExecute(script: this, dockerImage: 'docker:1.12.6-dind'){ 
+        sh "docker version"
     }
-     originalStage()
 }
 
 return this
