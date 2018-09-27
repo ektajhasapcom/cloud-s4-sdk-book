c563743cb2ed423756d13afdac24c700f9a6b4d4
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-27 13:35:43 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,22 +1,19 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
    
     
-    podTemplate(label: 'pod-hugo-app', containers: [
-    containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat',
-        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
-  ]) {
-
-    node('pod-hugo-app') {
-
-            container('docker') {
-                    sh("docker version")
-                    sh ("docker build -t ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER} .")
-                    sh ("docker push ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER}")
-                    sh ("docker tag ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:${env.BUILD_NUMBER} ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:latest")
-                    sh ("docker push ${DOCKER_HUB_ACCOUNT}/${DOCKER_IMAGE_NAME}:latest")
-        }        
-    }
-  }
+  podTemplate(label     : 'pod-hugo-app',
+            containers: [containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')]) {
+             node('pod-hugo-app') {
+                container(name: 'docker') {
+                    try {
+                        sh "docker version"
+                        sh "docker build ."
+                    } finally {
+                        echo "Finally"
+                     }
+                }
+            }
+        }
 }
 
 return this
