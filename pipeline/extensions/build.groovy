62bb67c70960c2d8bfb18f5e575c6a02976f333b
ektajhasapcom <42619694+ektajhasapcom@users.noreply.github.com>
2018-09-28 10:00:17 +0200

    Update build.groovy

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -10,13 +10,14 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
                   hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
              node('docker-node') {
                 container('maven') {
-                    checkout scm
-                    sh  "ls -lrt"
-                    originalStage()
+
                 }
                 container(name: 'docker') {
                     try {
                          withCredentials([usernamePassword(credentialsId: 'dockerCredentialId', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+                                checkout scm
+                                sh  "ls -lrt"
+                                originalStage()
                                 sh "ls -lrt"
                                 sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
                                 sh "docker build -t ${imageName}:${tag} ."
