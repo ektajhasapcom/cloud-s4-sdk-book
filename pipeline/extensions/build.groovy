e2e7ba9ff4af6460dd01e0ed014dcd2e8cf2ac1b
Ekta Jha <ekta.jha@sap.com>
2018-09-28 14:27:00 +0200

    Change(-): credentials
    Change-Id: I2145b28088b51c9096bf379762d881af332b9aca
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -11,14 +11,11 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
              node('docker-node') {
                 container('maven') {
                                 checkout scm
-                                sh  "ls -lrt"
                                 sh "mvn clean install"
                 }
                 container(name: 'docker') {
                     try {
                          withCredentials([usernamePassword(credentialsId: 'dockerCredentialId', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
-                                sh "chown -R 1000:1000 ."
-                                sh "ls -lrt"
                                 sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
                                 sh "docker build -t ${imageName}:${tag} ."
                                 sh "docker push ${imageName}:${tag}"     
--- pipeline/extensions/productionDeployment.groovy
+++ pipeline/extensions/productionDeployment.groovy
@@ -5,14 +5,14 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
      dockerExecute(script: this, dockerImage: 'ektajha/k8shelm:v1'){ 
         withCredentials([[
                 $class: 'FileBinding',
-                credentialsId: 'k8s-credentials',
+                credentialsId: 'K8s',
                 variable: 'KUBECONFIG'
             ]]){
                  sh "kubectl --kubeconfig=$KUBECONFIG"
                  sh "kubectl get pods"
                  sh "helm init --upgrade"
                  sh "sleep 40"
-                 sh "helm upgrade --install --force addrbook --set imageversion=$tag addressbook"            
+                 sh "helm upgrade --install --force addrbook --set imageversion=$tag addressbook --namespace=production"            
               }
     }
 
