857eef7cf01d14191c9d571646daf4288db255e5
Ekta Jha <ekta.jha@sap.com>
2018-10-01 18:52:11 +0200

    Change(Kubernetes):
    Change-Id: Id7ef80352c24c8e728d44741540d5d784e71d07f
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,31 +1,12 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
    
-  def  tag = "${env.BRANCH_NAME}.${env.BUILD_NUMBER}"
-  def imageName = "ektajha/addressbooklatest"
-
- 
-  podTemplate(label     : 'docker-node',
-            containers: [containerTemplate(name: 'maven', image: 'maven', ttyEnabled: true, command: 'cat'),
-                         containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')], volumes: [
-                  hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
-             node('docker-node') {
-                container('maven') {
-                                checkout scm
-                                sh "mvn clean install"
-                }
-                container(name: 'docker') {
-                    try {
-                         withCredentials([usernamePassword(credentialsId: 'dockerCredentialId', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
-                                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
-                                sh "docker build -t ${imageName}:${tag} ."
-                                sh "docker push ${imageName}:${tag}"     
-                         }
-                    } finally {
-                        echo "Finally"
-                     }
-                }
-            }
-        }
+    withKubeConfig([credentialsId: 'k8s-credentials',
+                    caCertificate: '<ca-certificate>',
+                    serverUrl: ''
+                    contextName: ''
+                    ]) {
+            sh 'kubectl get pods'
+    }
 }
 
 return this
