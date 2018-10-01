248c118c666bd63c5add8e8c8a3f902f76164884
Ekta Jha <ekta.jha@sap.com>
2018-10-01 19:35:17 +0200

    -
    Change-Id: I5f0e3ef77d1f534424f3f4744dda51fbc6d38658
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,14 +1,34 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
    
-   stage(stageName) {
-           withKubeConfig([credentialsId: 'k8s-credentials',
-                    caCertificate: '<ca-certificate>',
-                    serverUrl: '<api-server-address>',
-                    contextName: '<context-name>'
-                    ]) {
-                      sh "kubectl get pods"
-             }
-    }
+   podTemplate(label: 'build-node', yaml: """
+ 
+    apiVersion: build.knative.dev/v1alpha1
+    kind: Build
+    metadata:
+      name: addressbook
+      namespace: default
+    spec:
+      serviceAccountName: knative-build-service-account
+      source:      
+        git:
+          url: https://github.com/ektajhasapcom/cloud-s4-sdk-book.git
+          revision: master
+      steps:
+      - name: maven-build
+        image: maven
+        args: ['mvn','install']
+      - name: docker-build-and-push
+        image: gcr.io/kaniko-project/executor:v0.1.0
+        args:  #See kaniko docs for more info about the arguments passed
+        - --dockerfile=/workspace/Dockerfile
+        - --destination=docker.io/ektajha/addressbook 
+ 
+         """
+		 ) {
+             node('build-node') {
+                sh "Build and push"
+            }
+        }
 }
 
 return this
