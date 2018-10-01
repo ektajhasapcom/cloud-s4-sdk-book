91a3850236fea6119a8802fe00762c249260bd80
Ekta Jha <ekta.jha@sap.com>
2018-10-01 21:19:48 +0200

    -
    Change-Id: I107a3d6baff40c2e0d2eac24017f5cfbaf1ceed9
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,7 +1,7 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
      
-     dockerExecute(script: this, dockerImage: 'ektajha/k8shelm:v1'){ 
+     dockerExecute(script: this, dockerImage: 'ektajha/kubectl:v1'){ 
         withCredentials([[
                 $class: 'FileBinding',
                 credentialsId: 'k8s-credentials',
@@ -10,9 +10,7 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
                  sh "kubectl --kubeconfig=$KUBECONFIG"
                  sh "kubectl cluster-info"
                  sh "kubectl get pods"
-                 sh "helm init --upgrade"
-                 sh "sleep 10"
-                 sh "helm upgrade --install --force addrbook  addressbook"            
+          
               }
     }
 
