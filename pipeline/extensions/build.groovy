eb6fefa8a1b8a518753763e6fd4681df05f871d3
Ekta Jha <ekta.jha@sap.com>
2018-10-02 11:41:46 +0200

    Change(Knative Build)
    Change-Id: If338950f1ea1d2e41d21c0330a1d7d0b7aed0682
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,6 +1,7 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
      def  imageV = "${env.BRANCH_NAME}.${env.BUILD_NUMBER}"
+     def buildV = "${env.BUILD_NUMBER}"
      
      dockerExecute(script: this, dockerImage: 'ektajha/k8shelm:v1'){ 
         withCredentials([[
@@ -9,7 +10,7 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
                 variable: 'KUBECONFIG'
             ]]){
                  sh "kubectl --kubeconfig=$KUBECONFIG"          
-                 sh "helm upgrade --install --force buildconfiguration --set imageversion=$imageV buildconfiguration"            
+                 sh "helm upgrade --install --force buildconf --set buildVersion=$buildV --set imageversion=$imageV buildconfiguration"            
               }
     }
 
