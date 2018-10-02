20ba1e4c6f62ef434e3b966e48235b222ff3e52e
Ekta Jha <ekta.jha@sap.com>
2018-10-02 10:33:06 +0200

    Change(Build.yaml)
    Change-Id: Ie86c0ead9754f9aac77dcfc7a8f2bd6586f4c7a6
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -1,14 +1,13 @@
 def call(Closure originalStage, String stageName, Map stageConfiguration, Map generalConfiguration) {
     
      
-     dockerExecute(script: this, dockerImage: 'lachlanevenson/k8s-kubectl'){ 
+    dockerExecute(script: this, dockerImage: 'ektajha/k8shelm:v1'){ 
         withCredentials([[
                 $class: 'FileBinding',
                 credentialsId: 'k8s-credentials',
                 variable: 'KUBECONFIG'
             ]]){
                  sh "kubectl --kubeconfig=$KUBECONFIG"
-                 sh "kubectl delete -f build.yaml"
                  sh "kubectl apply -f build.yaml"           
               }
     }
