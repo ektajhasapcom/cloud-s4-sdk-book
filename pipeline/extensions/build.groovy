a1c13f7a58332073e19089c408454e31e0f9d430
Ekta Jha <ekta.jha@sap.com>
2018-10-02 10:27:13 +0200

    Change(Delete)
    Signed-off-by: Ekta Jha <ekta.jha@sap.com>

--- pipeline/extensions/build.groovy
+++ pipeline/extensions/build.groovy
@@ -8,6 +8,7 @@ def call(Closure originalStage, String stageName, Map stageConfiguration, Map ge
                 variable: 'KUBECONFIG'
             ]]){
                  sh "kubectl --kubeconfig=$KUBECONFIG"
+                 sh "kubectl delete -f build.yaml"
                  sh "kubectl apply -f build.yaml"           
               }
     }
